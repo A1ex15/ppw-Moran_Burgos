4d1a05d992bec8b0e0d322aca60eb58e8af074d9
Alexis Moran <99370628+A1ex15@users.noreply.github.com>
2022-03-06 00:06:09 -0500

    Update index.js

--- build/index.js
+++ build/index.js
@@ -5,7 +5,7 @@ var _express = _interopRequireDefault(require("express"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var app = (0, _express["default"])();
-app.listen(4000);
-console.log('Server listen on port', 4000); 
+app.listen(2000);
+console.log('Server listen on port', 2000); 
 
 
