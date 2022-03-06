a7cb7bdfb19b12468f84147c6e2f4950d297dee6
Alexis Moran <99370628+A1ex15@users.noreply.github.com>
2022-03-05 23:21:06 -0500

    commit

---
+++ src/controllers/products.controller.js
@@ -0,0 +1,23 @@
+import Product from '../models/Product'
+
+export const createProduct = (req, res) => {
+    res.json('creating product')   
+}
+
+export const getProducts = (req, res) => {
+    res.json("get products")
+    
+}
+
+export const getProductById = (req, res) => {
+    
+}
+
+export const updateProductById = (req, res) => {
+    
+}
+
+export const deleteProductById = (req, res) => {
+    
+}
+
